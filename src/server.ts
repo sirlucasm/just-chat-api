7bf17f0aba0d8f63e3d0d085498f8f1eacc2b676
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-08-24 22:48:21 -0300

    fix: fixed duplicated server
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import chalk from 'chalk';
-import app from './app';
 import { server } from './socket';
 
 const PORT = process.env.PORT || 8080;
-const SOCKET_PORT = process.env.SOCKET_PORT || 3333;
 
-server.listen(SOCKET_PORT, () => console.log(chalk`\n{green ⚡ [SOCKET] Listening on port ${SOCKET_PORT}}`));
-
-app.listen(PORT, () => console.log(chalk`\n{green ⚡ [SERVER] Listening on port ${PORT}}\n`));
+server.listen(PORT, () => console.log(chalk`\n{green ⚡ [SERVER] Listening on port ${PORT}}\n`));
